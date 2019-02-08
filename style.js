065ce2ad5501af9453e9af1fea3898cce3286b25
RaquelROES <32135998+RaquelROES@users.noreply.github.com>
2019-02-08 15:39:27 +0100

    Update style.js

--- style.js
+++ style.js
@@ -7,7 +7,7 @@
 		page = blink.currentPage;
 
 	al_denteStyle.prototype = {
-		bodyClassName: 'content_type_clase_al_dente',
+		bodyClassName: 'content_type_clase_poptropica',
 		ckEditorStyles: {
 			name: 'al_dente',
 			styles: [
