f14228b47615676bbf12782a528742f24ed4fc58
RaquelROES <32135998+RaquelROES@users.noreply.github.com>
2019-02-08 15:12:38 +0100

    Update style.js

--- style.js
+++ style.js
@@ -22,8 +22,7 @@
 
 				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box1' } },
 				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box2' } },
-				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box3' } }
-
+				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box3' } },
 			]
 		},
 
